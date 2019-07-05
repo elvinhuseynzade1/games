4b5825a6784a9cdc145a726e4b4c85a05502ad5e
Elvin <elvinhuseynzade1>
2019-07-05 16:05:03 +0400

    dff

---
+++ index.js
@@ -0,0 +1,6 @@
+
+var letters=['a','b','c','d','e','f','g','h','i','g','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+var computerGuess = letters[Math.floor(Math.random() * letters.length)];
+
+console.log(computerGuess)
